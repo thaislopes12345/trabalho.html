1af8112a9eb4ed15ea9e975d98a5cf1d9cef51ec
thaislopes12345 <thaisoliveira.lopes@aluno.educa.go.gov.br>
2025-05-30 16:46:55 -0300

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const question = [
+
+    {
+        question: "qual é maior animal do mundo?"
+        answers: [
+            
+                {id: 1, text:"tubarão", correct:false},
+                {id: 2, text:"tubarão", correct:false},
+                {id: 3, text:"tubarão", correct:false},
+                {id: 4, text:"tubarão", correct:false},
+                    
+        ]
+    }
+]
